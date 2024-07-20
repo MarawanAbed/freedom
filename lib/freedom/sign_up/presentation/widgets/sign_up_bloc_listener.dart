e2764723e7b0333a7f64ad0bce20a7acd2dbe4f6
marwan abed <marwanabed453@gmail.com>
2024-07-21 01:42:55 +0300

    finish sign up

---
+++ lib/freedom/sign_up/presentation/widgets/sign_up_bloc_listener.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:freedom_chat_app/core/helpers/helper_methods.dart';
+import 'package:freedom_chat_app/core/utils/strings.dart';
+import 'package:freedom_chat_app/freedom/sign_up/presentation/bloc/sign_up_cubit.dart';
+
+class SignUpBlocListener extends StatelessWidget {
+  const SignUpBlocListener({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<SignUpCubit, SignUpState>(
+      listenWhen: (previous, current) =>
+          current is Loading || current is Success || current is Error,
+      listener: (BuildContext context, state) {
+        state.whenOrNull(
+          loading: () {
+            HelperMethod.showLoadingDialog(context);
+          },
+          success: () {
+            Navigator.pop(context);
+            HelperMethod.showSuccessToast(
+              AppStrings.registerSuccess,
+              gravity: ToastGravity.BOTTOM,
+            );
+
+          },
+          error: (message) {
+            Navigator.pop(context);
+            HelperMethod.showErrorToast(message, gravity: ToastGravity.BOTTOM);
+          },
+        );
+      },
+      child: const SizedBox.shrink(),
+    );
+  }
+}
