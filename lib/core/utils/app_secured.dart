128bb2929b0107006ea9cc550ec5ff185e117f61
marwan abed <marwanabed453@gmail.com>
2024-07-25 21:08:07 +0300

    readme

--- lib/core/utils/app_secured.dart
+++ lib/core/utils/app_secured.dart
@@ -1,10 +1,11 @@
+
+
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class AppSecured {
-  static const String twitterApiKey = 'a3OKSWM5tLoE9lZ92cevS0lZK';
-  static const String twitterApiSecret =
-      'Sx0wZdNrK1ld9eiv1agAlCuqmwrs6tyqcYNExF4t9XunmsETWO';
-  static const String gitHubClientId = 'Ov23lin5siEv5rnhCVOQ';
-  static const String gitHubClientSecret =
-      'ebe846f4f807e3770e2841ab7a70d19c38400845';
-  static const String appMessaging =
-      'AAAAnqzCkZQ:APA91bGVTGo1VqbR5hTDgf0NK9p5sLkkOqDsi9ktY2wPJQSKoBbh5NHO8bWT4_5p4TEfEs8gq7BBU_A9ByCJtTyg-AISQUZJlpPS7iXbfCPjdRFn6bkJAyEuuo3hw7dihTy2n29-VG3Z';
+  static final String twitterApiKey = dotenv.env['TWITTER_API_KEY']!;
+  static final String twitterApiSecret = dotenv.env['TWITTER_API_SECRET']!;
+  static final String gitHubClientId = dotenv.env['GITHUB_CLIENT_ID']!;
+  static final String gitHubClientSecret = dotenv.env['GITHUB_CLIENT_SECRET']!;
+  static final String appMessaging = dotenv.env['APP_MESSAGING']!;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,14 @@ import 'package:freedom_chat_app/core/services/notification_services.dart';
 import 'package:freedom_chat_app/core/utils/strings.dart';
 import 'package:freedom_chat_app/firebase_options.dart';
 import 'package:freedom_chat_app/freedom.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await dotenv.load(fileName: ".env");
   await setupGetIt();
   await getIt<LocalNotificationServices>().initNotification();
   getIt<RemoteNotificationService>().firebaseNotification();
